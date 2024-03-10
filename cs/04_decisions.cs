8c74c396ffab12ff385fbef11f3f8f39623b7d4c
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2024-03-10 11:18:11 +0800

    if else and switch

---
+++ cs/04_decisions.cs
@@ -0,0 +1,39 @@
+﻿
+// sample value
+int balance = 100;
+
+// if else statement
+if (balance == 100)
+{
+    Console.WriteLine("the value is 100");
+}
+else
+{
+    Console.WriteLine("the value is not 100.\n");
+}
+
+// if else can be replaced by ternary operator
+Console.WriteLine(balance < 100 ? "the val is less than 100" : "the val is 100 and above");
+
+
+// sample value
+int studentGrade = 53;
+
+// switch statement
+switch (studentGrade)
+{
+    case 00:
+    case 59:
+        Console.WriteLine("failing grade");
+        break;
+    case 60:
+        Console.WriteLine("the val is 60");
+        break;
+    case 61:
+    case 100:
+        Console.WriteLine("passing grade");
+        break;
+    default:
+        Console.WriteLine("invalid grade");
+        break;
+}
